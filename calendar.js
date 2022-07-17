cd3cc9365482e1aa5656012fd2c6b8e15181d065
Jamie Spang <jamie_spang@yahoo.com>
2022-07-16 21:17:05 -0400

    updated JS and removed extra code that is not needed for weekly calendar.

--- calendar.js
+++ calendar.js
@@ -1,292 +1,4 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=imperial&mode=xml",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "4ea217fed2msh49eb425424a96c4p103595jsnc6798693c974",
-		"X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com"
-	}
-};
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
-
-function dragstart_handler(ev) {
-  console.log("dragStart");
-  // Add this element's id to the drag payload so the drop handler will
-  // know which element to add to its tree
-  const dataList = ev.dataTransfer.items;
-  dataList.add(ev.target.id, "text/plain");
-  // Add some other items to the drag payload
-  dataList.add("<p>... paragraph ...</p>", "text/html");
-  dataList.add("http://www.example.org","text/uri-list");
-}
-
-function drop_handler(ev) {
-  console.log("Drop");
-  ev.preventDefault();
-  const data = ev.dataTransfer.items;
-  // Loop through the dropped items and log their data
-  for (let i = 0; i < data.length; i++) {
-    if ((data[i].kind == 'string') && (data[i].type.match('^text/plain'))) {
-      // This item is the target node
-      data[i].getAsString(function (s){
-        ev.target.appendChild(document.getElementById(s));
-      });
-    } else if ((data[i].kind == 'string') && (data[i].type.match('^text/html'))) {
-      // Drag data item is HTML
-      data[i].getAsString(function (s){
-        console.log("... Drop: HTML = " + s);
-      });
-    } else if ((data[i].kind == 'string') && (data[i].type.match('^text/uri-list'))) {
-      // Drag data item is URI
-      data[i].getAsString(function (s){
-        console.log("... Drop: URI = " + s);
-      });
-    }
-  }
-}
-
-function dragover_handler(ev) {
-  console.log("dragOver");
-  ev.preventDefault();
-  // Set the dropEffect to move
-  ev.dataTransfer.dropEffect = "move"
-}
-
-function dragend_handler(ev) {
-  console.log("dragEnd");
-  const dataList = ev.dataTransfer.items;
-  // Clear any remaining drag data
-  dataList.clear();
-}
-//check the console for date click event
-//Fixed day highlight
-//Added previous month and next month view
-
-function CalendarControl() {
-    const calendar = new Date();
-    const calendarControl = {
-      localDate: new Date(),
-      prevMonthLastDate: null,
-      calWeekDays: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-      calMonthName: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-      ],
-      daysInMonth: function (month, year) {
-        return new Date(year, month, 0).getDate();
-      },
-      firstDay: function () {
-        return new Date(calendar.getFullYear(), calendar.getMonth(), 1);
-      },
-      lastDay: function () {
-        return new Date(calendar.getFullYear(), calendar.getMonth() + 1, 0);
-      },
-      firstDayNumber: function () {
-        return calendarControl.firstDay().getDay() + 1;
-      },
-      lastDayNumber: function () {
-        return calendarControl.lastDay().getDay() + 1;
-      },
-      getPreviousMonthLastDate: function () {
-        let lastDate = new Date(
-          calendar.getFullYear(),
-          calendar.getMonth(),
-          0
-        ).getDate();
-        return lastDate;
-      },
-      navigateToPreviousMonth: function () {
-        calendar.setMonth(calendar.getMonth() - 1);
-        calendarControl.attachEventsOnNextPrev();
-      },
-      navigateToNextMonth: function () {
-        calendar.setMonth(calendar.getMonth() + 1);
-        calendarControl.attachEventsOnNextPrev();
-      },
-      navigateToCurrentMonth: function () {
-        let currentMonth = calendarControl.localDate.getMonth();
-        let currentYear = calendarControl.localDate.getFullYear();
-        calendar.setMonth(currentMonth);
-        calendar.setYear(currentYear);
-        calendarControl.attachEventsOnNextPrev();
-      },
-      displayYear: function () {
-        let yearLabel = document.querySelector(".calendar .calendar-year-label");
-        yearLabel.innerHTML = calendar.getFullYear();
-      },
-      displayMonth: function () {
-        let monthLabel = document.querySelector(
-          ".calendar .calendar-month-label"
-        );
-        monthLabel.innerHTML = calendarControl.calMonthName[calendar.getMonth()];
-      },
-      selectDate: function (e) {
-        console.log(
-          `${e.target.textContent} ${
-            calendarControl.calMonthName[calendar.getMonth()]
-          } ${calendar.getFullYear()}`
-        );
-      },
-      plotSelectors: function () {
-        document.querySelector(
-          ".calendar"
-        ).innerHTML += `<div class="calendar-inner"><div class="calendar-controls">
-          <div class="calendar-prev"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128"><path fill="#666" d="M88.2 3.8L35.8 56.23 28 64l7.8 7.78 52.4 52.4 9.78-7.76L45.58 64l52.4-52.4z"/></svg></a></div>
-          <div class="calendar-year-month">
-          <div class="calendar-month-label"></div>
-          <div>-</div>
-          <div class="calendar-year-label"></div>
-          </div>
-          <div class="calendar-next"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128"><path fill="#666" d="M38.8 124.2l52.4-52.42L99 64l-7.77-7.78-52.4-52.4-9.8 7.77L81.44 64 29 116.42z"/></svg></a></div>
-          </div>
-          <div class="calendar-today-date">Today: 
-            ${calendarControl.calWeekDays[calendarControl.localDate.getDay()]}, 
-            ${calendarControl.localDate.getDate()}, 
-            ${calendarControl.calMonthName[calendarControl.localDate.getMonth()]} 
-            ${calendarControl.localDate.getFullYear()}
-          </div>
-          <div class="calendar-body"></div></div>`;
-      },
-      plotDayNames: function () {
-        for (let i = 0; i < calendarControl.calWeekDays.length; i++) {
-          document.querySelector(
-            ".calendar .calendar-body"
-          ).innerHTML += `<div>${calendarControl.calWeekDays[i]}</div>`;
-        }
-      },
-      plotDates: function () {
-        document.querySelector(".calendar .calendar-body").innerHTML = "";
-        calendarControl.plotDayNames();
-        calendarControl.displayMonth();
-        calendarControl.displayYear();
-        let count = 1;
-        let prevDateCount = 0;
-  
-        calendarControl.prevMonthLastDate = calendarControl.getPreviousMonthLastDate();
-        let prevMonthDatesArray = [];
-        let calendarDays = calendarControl.daysInMonth(
-          calendar.getMonth() + 1,
-          calendar.getFullYear()
-        );
-        // dates of current month
-        for (let i = 1; i < calendarDays; i++) {
-          if (i < calendarControl.firstDayNumber()) {
-            prevDateCount += 1;
-            document.querySelector(
-              ".calendar .calendar-body"
-            ).innerHTML += `<div class="prev-dates"></div>`;
-            prevMonthDatesArray.push(calendarControl.prevMonthLastDate--);
-          } else {
-            document.querySelector(
-              ".calendar .calendar-body"
-            ).innerHTML += `<div class="number-item" data-num=${count}><a class="dateNumber" href="#">${count++}</a></div>`;
-          }
-        }
-        //remaining dates after month dates
-        for (let j = 0; j < prevDateCount + 1; j++) {
-          document.querySelector(
-            ".calendar .calendar-body"
-          ).innerHTML += `<div class="number-item" data-num=${count}><a class="dateNumber" href="#">${count++}</a></div>`;
-        }
-        calendarControl.highlightToday();
-        calendarControl.plotPrevMonthDates(prevMonthDatesArray);
-        calendarControl.plotNextMonthDates();
-      },
-      attachEvents: function () {
-        let prevBtn = document.querySelector(".calendar .calendar-prev a");
-        let nextBtn = document.querySelector(".calendar .calendar-next a");
-        let todayDate = document.querySelector(".calendar .calendar-today-date");
-        let dateNumber = document.querySelectorAll(".calendar .dateNumber");
-        prevBtn.addEventListener(
-          "click",
-          calendarControl.navigateToPreviousMonth
-        );
-        nextBtn.addEventListener("click", calendarControl.navigateToNextMonth);
-        todayDate.addEventListener(
-          "click",
-          calendarControl.navigateToCurrentMonth
-        );
-        for (var i = 0; i < dateNumber.length; i++) {
-            dateNumber[i].addEventListener(
-              "click",
-              calendarControl.selectDate,
-              false
-            );
-        }
-      },
-      highlightToday: function () {
-        let currentMonth = calendarControl.localDate.getMonth() + 1;
-        let changedMonth = calendar.getMonth() + 1;
-        let currentYear = calendarControl.localDate.getFullYear();
-        let changedYear = calendar.getFullYear();
-        if (
-          currentYear === changedYear &&
-          currentMonth === changedMonth &&
-          document.querySelectorAll(".number-item")
-        ) {
-          document
-            .querySelectorAll(".number-item")
-            [calendar.getDate() - 1].classList.add("calendar-today");
-        }
-      },
-      plotPrevMonthDates: function(dates){
-        dates.reverse();
-        for(let i=0;i<dates.length;i++) {
-            if(document.querySelectorAll(".prev-dates")) {
-                document.querySelectorAll(".prev-dates")[i].textContent = dates[i];
-            }
-        }
-      },
-      plotNextMonthDates: function(){
-       let childElemCount = document.querySelector('.calendar-body').childElementCount;
-       //7 lines
-       if(childElemCount > 42 ) {
-           let diff = 49 - childElemCount;
-           calendarControl.loopThroughNextDays(diff);
-       }
-
-       //6 lines
-       if(childElemCount > 35 && childElemCount <= 42 ) {
-        let diff = 42 - childElemCount;
-        calendarControl.loopThroughNextDays(42 - childElemCount);
-       }
-
-      },
-      loopThroughNextDays: function(count) {
-        if(count > 0) {
-            for(let i=1;i<=count;i++) {
-                document.querySelector('.calendar-body').innerHTML += `<div class="next-dates">${i}</div>`;
-            }
-        }
-      },
-      attachEventsOnNextPrev: function () {
-        calendarControl.plotDates();
-        calendarControl.attachEvents();
-      },
-      init: function () {
-        calendarControl.plotSelectors();
-        calendarControl.plotDates();
-        calendarControl.attachEvents();
-      }
-    };
-    calendarControl.init();
-  }
-  
   const calendarControl = new CalendarControl();
 // Create a new chore when clicking on the "Add" button
 function newElement() {
@@ -314,75 +26,65 @@ function newElement() {
     }
   }
 }
-var selectedObjs;
-var draggableOptions = {
-  helper: function() {
-    return $("<div class='Dragbox'> You have selected" + $('div.selected').length + "Item <div>");
-  },
-  cursor: "move",
-  cursorAt: {
-    top: 10,
-    left: 50
-  },
 
-  start: function() {
-    selectedObjs = $('div.selected');
-  }
-};
 
-$(".checkbox-drag").click(function() {
-  if ($(this).is(":checked")) {
-    $(this).closest(".draggable").addClass("selected");
-  } else {
-    $(this).closest(".draggable").removeClass("selected");
-  }
+// Display today's day and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
-  var selectedObjects = $('div.selected');
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-  $(".draggable").draggable();
-  if (selectedObjects.length > 0) {
-    $(".draggable").draggable("destroy");
-    $(selectedObjects).draggable(draggableOptions);
-  } else {
-    $(".draggable").draggable("destroy");
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+   
+    function timeTracker() {
+        //get current number of hours.
+        var timeNow = moment().hour();
 
-  }
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-});
-$('.droppable').droppable({
-  hoverClass: "darggablehover",
-  drop: function(event, ui) {
-    counterEle = $(this).find(".counter");
-    counter = Number(counterEle.text()) + selectedObjs.length;
-    $(counterEle).html(counter);
-    $(selectedObjs).hide("slow", function() {
-      $(selectedObjs).remove();
-    });
-  }
-});
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+            }
+        })
     }
-  }
-}
+
+    // Get item from local storage if any
+    $("#hour7 .description").val(localStorage.getItem("hour7"));
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+    $("#hour19 .description").val(localStorage.getItem("hour19"));
+
+    timeTracker();
+})
