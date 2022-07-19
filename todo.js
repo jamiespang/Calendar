54900cda834192ba02b50aa77f4b21e63231aa18
Jamie Spang <jamie_spang@yahoo.com>
2022-07-19 19:19:46 -0400

    updated JS and to do list is still not working

--- todo.js
+++ todo.js
@@ -1,16 +1,44 @@
 //to do list//
-const todoObjectsList = []
-constructor (item){
-this.ulElement=item}
-addEventListener(){""
-}
-done_undone(x){""}
-deleteElement(z){""
-display(){""}}
+const todoObjectList = []
 
-const listSection=document.jquery.Selector("#myUl"); 
-myTodoList=new todo_class
-(listSelection); 
-document.querySelector(".addBtn")
-.addEventListener("click", function(){
-myTodoList.add()})
+class Todo_Class{
+constructor(item){
+this.ulElement=item; 
+}
+add() {""
+}
+done_undone(x) {""
+}
+deleteElement(z) {""
+}
+
+display() {""
+}
+
+}
+
+const listSection= document.querySelector("#myUL"); 
+
+myTodoList = new Todo_Class(listSection); 
+
+document.querySelector(".addBtn").addEventListener("click", function() {
+myTodoList.add()
+})
+
+add() {
+const todoInput=document.querySelector("#myInput").ariaValueMax; 
+if (todoInput == "") {
+alert("You did not enter an item!)
+}else {
+const todoObject = {
+id: todoObjectList.length, 
+todoText: todoInput, 
+isDone: false, 
+}
+todoObjectList.unshift(todoObject); 
+this.display(); 
+document.querySelector("#myInput").value=''; 
+
+	}
+
+}
