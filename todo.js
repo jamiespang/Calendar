c7ebdd459bfa0e2f793a3425b526abbb6b710a88
Jamie Spang <jamie_spang@yahoo.com>
2022-07-19 19:10:03 -0400

    redo the to do list items and update js

---
+++ todo.js
@@ -0,0 +1,16 @@
+//to do list//
+const todoObjectsList = []
+constructor (item){
+this.ulElement=item}
+addEventListener(){""
+}
+done_undone(x){""}
+deleteElement(z){""
+display(){""}}
+
+const listSection=document.jquery.Selector("#myUl"); 
+myTodoList=new todo_class
+(listSelection); 
+document.querySelector(".addBtn")
+.addEventListener("click", function(){
+myTodoList.add()})
