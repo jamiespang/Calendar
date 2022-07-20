f4c58804bc5aa3edfa607f93052addd53ce764c1
Jamie Spang <jamie_spang@yahoo.com>
2022-07-19 21:29:52 -0400

    updated js for to do list

--- todo.js
+++ todo.js
@@ -1,44 +1,87 @@
-//to do list//
-const todoObjectList = []
 
-class Todo_Class{
-constructor(item){
-this.ulElement=item; 
+const todoList = [];
+
+const todoListElement = document.querySelector("#myUL");
+
+document.querySelector("#add_button").addEventListener("click", addTodo);
+document.querySelector("#myInput").addEventListener("keydown", function(e) {
+  if (e.keyCode == 13) {
+    addTodo()
+  }
+});
+
+//-------GETTING VALUES FROM INPUT TO ARRAY OF OBJECTS-------
+function addTodo() {
+  const todoText = document.querySelector("#myInput").value;
+
+  if (todoText == "") {
+    alert("You did not enter any item");
+  } else {
+    const todoObject = {
+      id: todoList.length,
+      todoText: todoText,
+      isDone: false,
+    };
+
+    //---WITH UNSHIFT WE ADD THE NEW ELEMENT TO THE BEGINNING OF THE ARRAY
+    //--SO THAT THE NEW ITEMS SHOW UP ON TOP
+    todoList.unshift(todoObject);
+    displayTodos();
+  }
 }
-add() {""
+
+//------CHANGING THE isDone VALUE TO TRUE WHEN THE ELEMENT IS CLICKED
+//------OR TO FALSE IF IT WAS TRUE BEFORE
+function doneTodo(todoId) {
+  const selectedTodoIndex = todoList.findIndex((item) => item.id == todoId);
+
+  todoList[selectedTodoIndex].isDone
+    ? (todoList[selectedTodoIndex].isDone = false)
+    : (todoList[selectedTodoIndex].isDone = true);
+  displayTodos();
 }
-done_undone(x) {""
+
+//----TO DELETE AN ITEM; FROM THE LIST
+function deleteItem(x) {
+  todoList.splice(
+    todoList.findIndex((item) => item.id == x),
+    1
+  );
+  displayTodos();
 }
-deleteElement(z) {""
-}
 
-display() {""
-}
+//---------DISPLAYING THE ENTERED ITEMS ON THE SCREEN------
+function displayTodos() {
+  todoListElement.innerHTML = "";
+  document.querySelector("#myInput").value = "";
 
-}
+  todoList.forEach((item) => {
+    const listElement = document.createElement("li");
+    const delBtn = document.createElement("i");
 
-const listSection= document.querySelector("#myUL"); 
+    listElement.innerHTML = item.todoText;
+    listElement.setAttribute("data-id", item.id);
 
-myTodoList = new Todo_Class(listSection); 
+    delBtn.setAttribute("data-id", item.id);
+    delBtn.classList.add("far");
+    delBtn.classList.add("fa-trash-alt");
+    delBtn.setAttribute("data-id", item.id);
 
-document.querySelector(".addBtn").addEventListener("click", function() {
-myTodoList.add()
-})
+    if (item.isDone) {
+      listElement.classList.add("checked");
+    }
 
-add() {
-const todoInput=document.querySelector("#myInput").ariaValueMax; 
-if (todoInput == "") {
-alert("You did not enter an item!)
-}else {
-const todoObject = {
-id: todoObjectList.length, 
-todoText: todoInput, 
-isDone: false, 
-}
-todoObjectList.unshift(todoObject); 
-this.display(); 
-document.querySelector("#myInput").value=''; 
+    listElement.addEventListener("click", function (e) {
+      const selectedId = e.target.getAttribute("data-id");
+      doneTodo(selectedId);
+    });
 
-	}
+    delBtn.addEventListener("click", function (e) {
+      const delId = e.target.getAttribute("data-id");
+      deleteItem(delId);
+    });
 
+    todoListElement.appendChild(listElement);
+    listElement.appendChild(delBtn);
+  });
 }
